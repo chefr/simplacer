63aa0d5483f017b73bde064b9286f398071ffff3
Chefranov Roman <roman_chefr@bk.ru>
2020-01-20 01:38:32 +0300

    Initial commit

---
+++ src/main/java/ru/chefranov/simplacer/Simplacer.java
@@ -0,0 +1,8 @@
+package ru.chefranov.simplacer;
+
+public class Simplacer {
+
+    public static void main( String[] args ) {
+        System.out.println( "Simplacer" );
+    }
+}
---
+++ src/test/java/ru/chefranov/simplacer/SimplacerTest.java
@@ -0,0 +1,14 @@
+package ru.chefranov.simplacer;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class SimplacerTest
+{
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue( true );
+    }
+}
